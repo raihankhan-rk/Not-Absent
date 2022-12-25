08d80198d644433fdff84cd7309d2b0c4538e952
Raihan Khan <iamrk98@gmail.com>
2022-12-25 15:49:50 +0530

    added creds.json

--- app.py
+++ app.py
@@ -14,9 +14,8 @@ account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
-#{'type': os.getenv('type'), 'project_id': os.getenv('project_id'), 'private_key_id': os.getenv('private_key_id'), 'private_key': os.getenv('private_key'), 'client_email': os.getenv('client_email'), 'client_id': os.getenv('client_id'), 'auth_uri': os.getenv('auth_uri'), 'token_uri': os.getenv('token_uri'), 'auth_provider_x509_cert_url': os.getenv('auth_provider_x509_cert_url'), 'client_x509_cert_url': os.getenv('client_x509_cert_url')}
 
-cred = credentials.Certificate(os.getenv('FIREBASE_KEY'))
+cred = credentials.Certificate('creds.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
